917c78c785c0c9c07b4dea4a785515355359fedb
nijat21 <nijat.ismayilov@outlook.com>
2025-11-03 20:18:21 +0000

    small leak determined and fixed for empty map files

--- fdf.c
+++ fdf.c
@@ -6,26 +6,19 @@
 /*   By: nismayil <nismayil@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 00:18:55 by nismayil          #+#    #+#             */
-/*   Updated: 2025/11/03 13:00:09 by nismayil         ###   ########.fr       */
+/*   Updated: 2025/11/03 20:15:13 by nismayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/*
-****  REFACTOR, TEST AND DEBUG
-****  BUFFERING
-****  PROXIMATION TO THE SAMPLE FDF_LINUX
-*/
-
-t_image	*img_init(t_win *win, int fd)
+t_image	*img_init(t_win *win)
 {
 	t_image	*img;
 
 	img = malloc(sizeof(t_image));
 	if (!img)
 	{
-		close(fd);
 		free(win);
 		exit_msg("Image couldn't be allocated\n");
 	}
@@ -34,7 +27,6 @@ t_image	*img_init(t_win *win, int fd)
 	img->img_ptr = mlx_new_image(win->mlx, img->width, img->height);
 	if (!img->img_ptr)
 	{
-		close(fd);
 		free(win);
 		free(img);
 		exit_msg("Couldn't create image\n");
@@ -42,29 +34,22 @@ t_image	*img_init(t_win *win, int fd)
 	return (img);
 }
 
-t_win	*win_init(int fd)
+t_win	*win_init(void)
 {
 	t_win	*win;
 
 	win = malloc(sizeof(t_win));
 	if (!win)
-	{
-		close(fd);
 		exit_msg("Window couldn't be allocated\n");
-	}
 	win->mlx = mlx_init();
 	if (!win->mlx)
-	{
-		close(fd);
 		exit_msg("Issue with initiating Mlx\n");
-	}
 	win->width = 1920;
 	win->height = 1400;
-	win->win = mlx_new_window(win->mlx, win->width, win->height,
-			"nismayil's fdf");
+	win->win = mlx_new_window(win->mlx, win->width, win->height, "fdf");
 	if (!win->win)
 	{
-		close(fd);
+		free(win);
 		exit_msg("Couldn't create window\n");
 	}
 	mlx_clear_window(win->mlx, win->win);
@@ -81,15 +66,14 @@ int	main(int ac, char *av[])
 	if (ac != 2)
 		return (0);
 	fd = safe_open(av[1], O_RDONLY);
-	win = win_init(fd);
-	img = img_init(win, fd);
 	map = parse_store_map(fd);
 	if (!map)
 	{
-		free(img);
-		free(win);
+		ft_putstr_fd("Error: Invalid map\n", 2);
 		return (0);
 	}
+	win = win_init();
+	img = img_init(win);
 	draw_put_image(win, img, map);
 	mlx_key_hook(win->win, key_hook, win);
 	mlx_hook(win->win, 17, 1L << 17, close_win, win);
--- hooks.c
+++ hooks.c
@@ -6,15 +6,12 @@
 /*   By: nismayil <nismayil@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 00:18:30 by nismayil          #+#    #+#             */
-/*   Updated: 2025/11/03 18:17:47 by nismayil         ###   ########.fr       */
+/*   Updated: 2025/11/03 20:07:53 by nismayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-/* If ESC cliced, close the window in a clean way */
-/* In MacOS, window is automatically closed when X button is clicked.
-In Linux, it should be handled manually */
 int	key_hook(int keycode, t_win *win)
 {
 	if (keycode == 53 || keycode == 65307)
